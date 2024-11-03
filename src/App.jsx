90571ee5f3cf64c0001bc081820313c90677d198
Soumil Biswas <160828589+Soumil-Biswas-Web@users.noreply.github.com>
2024-11-03 11:07:38 +0530

    Added Header to Web Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Header } from "./modules/Header.jsx";
+import Header from "./modules/Header.jsx";
 import "./styles.css";
 import ExpForm from "./modules/ExperienceForm.jsx";
 import EduForm from "./modules/EducationForm.jsx";
@@ -77,52 +77,54 @@ export default function App() {
 
   return (
     <div className="App">
-      {/* <Header /> */}
-      <div className="Forms">
-        <GenForm saveGenInfo={saveGenInfo} />
-        <EduForm saveEduInfo={saveEduInfo} />
-        <ExpForm saveExpInfo={saveExpInfo} />
-      </div>
-      <div className="panel">
-        <button className="print_button" onClick={printPage}>
-          Print Resume
-        </button>
-        <span id="HeadSpan"></span>
-        <div className="Page">        
-          <ResumePage
-            name={genInfo.name}
-            address={genInfo.address}
-            email={genInfo.email}
-            phone={genInfo.phone}
-            languages={genInfo.languages}
-            links={genInfo.links}
-          />
-          {eduInfo.map((edu, index) => {
-            return (
-              <EduPanel
-                lvl={edu.lvl}
-                inst={edu.inst}
-                dateStart={edu.dateStart}
-                dateEnd={edu.dateEnd}
-                gradeType={edu.gradeType}
-                grade={edu.grade}
-                key={index}
-              />
-            );
-          })}
-          {expInfo.map((exp, index) => {
-            return (
-              <ExpPanel
-                compName={exp.compName}
-                compPos={exp.compPos}
-                dateStart={exp.dateStart}
-                dateEnd={exp.dateEnd}
-                point={exp.point}
-                skils={exp.skills}
-                key={index}
-              />
-            );
-          })}
+      <Header />
+      <div className="content">
+        <div className="Forms">
+          <GenForm saveGenInfo={saveGenInfo} />
+          <EduForm saveEduInfo={saveEduInfo} />
+          <ExpForm saveExpInfo={saveExpInfo} />
+        </div>
+        <div className="panel">
+          <button className="print_button" onClick={printPage}>
+            Print Resume
+          </button>
+          <span id="HeadSpan"></span>
+          <div className="Page">        
+            <ResumePage
+              name={genInfo.name}
+              address={genInfo.address}
+              email={genInfo.email}
+              phone={genInfo.phone}
+              languages={genInfo.languages}
+              links={genInfo.links}
+            />
+            {eduInfo.map((edu, index) => {
+              return (
+                <EduPanel
+                  lvl={edu.lvl}
+                  inst={edu.inst}
+                  dateStart={edu.dateStart}
+                  dateEnd={edu.dateEnd}
+                  gradeType={edu.gradeType}
+                  grade={edu.grade}
+                  key={index}
+                />
+              );
+            })}
+            {expInfo.map((exp, index) => {
+              return (
+                <ExpPanel
+                  compName={exp.compName}
+                  compPos={exp.compPos}
+                  dateStart={exp.dateStart}
+                  dateEnd={exp.dateEnd}
+                  point={exp.point}
+                  skils={exp.skills}
+                  key={index}
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
--- src/modules/Header.jsx
+++ src/modules/Header.jsx
@@ -1,53 +1,10 @@
 import "../styles.css";
 
-let Forms;
-
-function GetForms() {
-  Forms = document.querySelector(".Forms").childnodes;
-  // console.log(Forms);
-}
-
-function TabButton({ id = "", text = "", target = "" }) {
-  const onClick = () => {
-    Forms.forEach(function (currentValue, currentIndex, listObj) {
-      if (currentValue.className === target) {
-        // console.log(currentIndex + " " + currentValue.style.display);
-        currentValue.style.display = "flex";
-      } else {
-        // console.log(currentIndex + " " + currentValue.style.display);
-        currentValue.style.display = "none";
-      }
-    });
-  };
-
+export default function Header () {
   return (
-    <button onClick={onClick} id={id}>
-      {text}
-    </button>
-  );
-}
-
-function Header() {
-  return (
-    <div className="Header">
-      <h1>Odin CV Application</h1>
-      <h2>Click on a tab and start entering your details!</h2>
-
-      {/* <div className="ButtonsTab">
-        <TabButton id="GenBtn" text="General Information" target="Gen Form" />
-        <TabButton id="EduBtn" text="Education Information" target="Edu Form" />
-        <TabButton
-          id="ExpBtn"
-          text="Experience Information"
-          target="Exp Form"
-        />
-      </div> */}
+    <div className="header">
+      <img src="https://i.imgur.com/jkwFqfX.png" alt="Page_Icon" />
+      <h1 id="headerText">Odin CV App</h1>
     </div>
-  );
+  )
 }
-
-export { Header, GetForms };
-
-// setTimeout(() => {
-//   GetForms();
-// }, 1000);
